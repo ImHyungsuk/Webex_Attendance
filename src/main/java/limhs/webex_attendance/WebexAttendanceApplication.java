de4ac00d72e89d4d7db4cb4e05b7dbc3dbe9524c
LimHyeongseok <limhs8630@gmail.com>
2024-07-29 17:13:24 +0900

    Init Project

---
+++ src/main/java/limhs/webex_attendance/WebexAttendanceApplication.java
@@ -0,0 +1,13 @@
+package limhs.webex_attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebexAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebexAttendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/limhs/webex_attendance/WebexAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package limhs.webex_attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebexAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
